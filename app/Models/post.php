ec5ecc78055f05398757b6817f49059c1ad51daa
Alexander <kkilonq@vk.com>
2026-04-24 04:34:15 -0700

    zxc

---
+++ app/Models/post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    protected $table ='posts';
+    protected $fillable = ['title','desc','text','date'];
+
+}
